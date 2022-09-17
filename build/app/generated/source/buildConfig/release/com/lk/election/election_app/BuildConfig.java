e7945a89652ca2c178cbcacf0e5c4dfd2bce5bba
Rusiru Wijethilake <rusirumc@gmail.com>
2022-09-18 03:27:01 +0530

    Initial Commit

---
+++ build/app/generated/source/buildConfig/release/com/lk/election/election_app/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.lk.election.election_app;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String APPLICATION_ID = "com.lk.election.election_app";
+  public static final String BUILD_TYPE = "release";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0.0";
+}
